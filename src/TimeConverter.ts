e4ade59708d3cf193e90829b8e7f534d091d9ff7
Sasank Vishnubhatla <sasank@vishnubhatlas.net>
2022-04-12 16:12:13 -0400

    time and frame conversion

--- src/TimeConverter.ts
+++ src/TimeConverter.ts
@@ -1,9 +1,56 @@
 import { SlippiGame } from '@slippi/slippi-js';
 
 class TimeConverter {
-  game: SlippiGame;
+  game: SlippiGame | null;
 
-  constructor(file: string) {
-    this.game = new SlippiGame(file);
+  constructor(file: string, _gameCheck = true) {
+    this.game = _gameCheck ? new SlippiGame(file) : null;
+  }
+
+  toFrames(time: string): number {
+    const TIME_REGEX_EXP = '([0-9]{1,2}):([0-9]{2})';
+    const tokens: RegExpMatchArray | null = time.match(TIME_REGEX_EXP);
+
+    if (tokens == null) {
+      throw new Error(
+        `Invalid input time (input: ${time}) passed into 'toFrames' within TimeConverter`
+      );
+    }
+
+    // tokens is structured as follows
+    // [ <time>, <minutes>, <seconds>, index: <starting index>, input: <time>, groups: undefined ]
+    const minutes = Number(tokens[1]);
+    const seconds = Number(tokens[2]);
+
+    const total_seconds = seconds + minutes * 60;
+    const frames = total_seconds * 60;
+
+    return frames;
+  }
+
+  toTime(frame: string): string {
+    const FRAME_REGEX_EXP = '([0-9]*)';
+    const tokens: RegExpMatchArray | null = frame.match(FRAME_REGEX_EXP);
+
+    if (tokens == null) {
+      throw new Error(
+        `Invalid frame (input: ${frame}) passed into 'toTime' within TimeConverter`
+      );
+    }
+
+    // tokens is structured as follows
+    // [ <frame>, <frame>, index: <starting index>, input: <frame>, groups: undefined ]
+    const n_frame = Number(tokens[1]);
+
+    // 60 frames/second
+    const total_seconds = ~~(n_frame / 60);
+    const minutes = ~~(total_seconds / 60);
+    const seconds = total_seconds % 60;
+
+    const time = `${minutes}:` + (seconds < 10 ? `0${seconds}` : `${seconds}`);
+
+    return time;
   }
 }
+
+export default TimeConverter;
--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,15 @@
-const message = 'this is a message';
-console.log(message);
+import TimeConverter from './TimeConverter';
+
+const fname = 'slippis/test.slp';
+const tc: TimeConverter = new TimeConverter(fname);
+
+console.log(tc.toFrames('00:00'));
+console.log(tc.toFrames('1:23'));
+console.log(tc.toFrames('45:67'));
+
+console.log(tc.toTime('0'));
+console.log(tc.toTime('12'));
+console.log(tc.toTime('345'));
+console.log(tc.toTime('6789'));
+console.log(tc.toTime('01234'));
+console.log(tc.toTime('567890'));
