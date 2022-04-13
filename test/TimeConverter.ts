7595863b0201cf28343f77a02fba6ce62219a74f
Sasank Vishnubhatla <sasank@vishnubhatlas.net>
2022-04-13 18:01:26 -0400

    basic 100 random tests. need to make random number generation weighted closer towards ingame times

--- test/TimeConverter.ts
+++ test/TimeConverter.ts
@@ -1,5 +1,19 @@
+import { FrameEntryType, SlippiGame } from '@slippi/slippi-js';
 import assert from 'assert';
 import TimeConverter from '../src/TimeConverter';
+
+function randtime(): [string, number] {
+  const minute: number = Math.floor(Math.random() * 10 + 1);
+  const second: number = Math.floor(Math.random() * 60);
+
+  const frames: number = (second + minute * 60) * 60;
+
+  return [
+    (minute < 10 ? `0${minute}:` : `${minute}:`) +
+      (second < 10 ? `0${second}` : `${second}`),
+    frames
+  ];
+}
 
 export function tc_tests() {
   describe('Time Converter Tests', () => {
@@ -7,29 +21,29 @@ export function tc_tests() {
 
     describe('actual file with game check enabled', () => {
       const tc1 = new TimeConverter(fname, true);
-      it(`$tc1 != null`, () => {
+      it(`tc1 != null`, () => {
         assert.notEqual(tc1, null);
       });
 
+      // todo get the last frame number
+      const sg: SlippiGame = new SlippiGame(fname);
+      const lastframe: FrameEntryType | null = sg.getLatestFrame();
+      it(`tc1->lastframe != null`, () => {
+        assert.notEqual(lastframe, null);
+      });
+
+      const last: number = lastframe == null ? 0 : lastframe.frame;
+
       describe('#toFrames', () => {
-        const zeroElapsed: number | undefined = tc1.toFrames('00:00');
-        it(`tc1 @ time 00:00 elapsed = 0`, () => {
-          assert.equal(zeroElapsed, 0);
-        });
-
-        const oneElapsed: number | undefined = tc1.toFrames('01:00');
-        it(`tc1 @ time 01:00 elapsed = 3600`, () => {
-          assert.equal(oneElapsed, 3600);
-        });
-
-        const twoElapsed: number | undefined = tc1.toFrames('02:00');
-        it(`tc1 @ time 02:00 elapsed = 7200`, () => {
-          assert.equal(twoElapsed, 7200);
-        });
-
-        const tenElapsed: number | undefined = tc1.toFrames('10:00');
-        it(`tc1 @ time 10:00 elapsed = undefined`, () => {
-          assert.equal(tenElapsed, undefined);
+        const looper: number[] = Array.from(Array(100).keys());
+        looper.forEach((idx: number) => {
+          const [r_time, _expected]: [string, number] = randtime();
+          const expected: number | undefined =
+            _expected < last ? _expected : undefined;
+          const elapsed: number | undefined = tc1.toFrames(r_time);
+          it(`test #${idx}: tc1 @ time ${r_time} elapsed = ${expected}`, () => {
+            assert.equal(elapsed, expected);
+          });
         });
       });
 
@@ -38,6 +52,13 @@ export function tc_tests() {
         it(`tc1 @ frame 0 = 00:00`, () => {
           assert.equal(zeroFrame, '00:00');
         });
+
+        const hundredFrame: string | undefined = tc1.toTime('100');
+        it(`tc1 @ frame 100 = 00:01`, () => {
+          assert.equal(hundredFrame, '00:01');
+        });
+
+        // TODO: random number of random time tests
       });
     });
   });
